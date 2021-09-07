012aea1c0527a92b8674386b6edeaff524a74fb0
Jachdich <jamesedkitching@gmail.com>
2021-09-07 21:14:27 +0100

    fuck knows what's going on

---
+++ src/com/cospox/csgoplugin/Main.java
@@ -0,0 +1,12 @@
+package com.cospox.csgoplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+    @Override
+    public void onDisable() {
+    }
+}

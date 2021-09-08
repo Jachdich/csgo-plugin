1b85c49d2e01c64c48915f2d0bb634f1c1558c1b
Jachdich <jamesedkitching@gmail.com>
2021-09-08 21:17:01 +0100

    no idea

--- src/com/cospox/csgoplugin/Main.java
+++ src/com/cospox/csgoplugin/Main.java
@@ -1,11 +1,17 @@
 package com.cospox.csgoplugin;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
     }
+    
     @Override
     public void onDisable() {
     }
